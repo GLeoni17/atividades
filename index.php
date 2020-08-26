7033fd3af748b665c25ddb083b891fdec6525a64
GLeoni17 <gabriel.lduarte@outlook.com>
2020-08-26 16:31:15 -0300

    Modificado index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<body>
 		<h1>Pagina com links das minhas atividades.</h1>
 		<a href="20200811/editor_texto/index.php">Exercicio Editor Texto c/ jQuery</a> <br>
-		<a href="20200819/Engenharia_Reversa/index.php">Exercicio Engenharia Reversa</a>
+		<a href="20200819/Engenharia_Reversa/index.php">Exercicio Engenharia Reversa</a> <br>
+		<a href="20200825/CadastroAjax.php">Exercicio Frutas Ajax</a>
 	</body>
 </html>
