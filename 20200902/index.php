139b7e94c05b12b07e02ae6fa7569512a684765d
GLeoni17 <gabriel.lduarte@outlook.com>
2020-09-06 20:59:42 -0300

    Adicionado Trabalho Bimestral - Gabriel Leoni e Renan Dias

--- 20200902/index.php
+++ 20200902/index.php
@@ -6,27 +6,73 @@
         <link href="bootstrap-4.4.1-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
         <script>
             $(document).ready(function(){
-                $("input[name='cep']").blur(function(){
+                $("#btn").click(function(){
                     var cep=$("input[name='cep']").val();
                     $.getJSON("https://viacep.com.br/ws/"+cep+"/json/", function(valores){
-                        $("#endereco").val(valores.logradouro)
-                        $("#numero").val(valores.complemento)
-                        $("#bairro").val(valores.bairro)
-                        $("#cidade").val(valores.localidade)
-                        $("#estado").val(valores.uf)
+                        if(!("erro" in valores)){
+                            $("#endereco").val(valores.logradouro)
+                            $("#complemento").val(valores.complemento)
+                            $("#bairro").val(valores.bairro)
+                            $("#cidade").val(valores.localidade)
+                            $("#estado").val(valores.uf)
+                        }else{
+                            alert("Endereço não encontrado!");
+                            $("#cep").val("");
+                        }
+                        
                     });
                 });
             });
         </script>
+        <style>
+            .centralizacep, .centralizabtn, .centralizaend, .centralizacomp, .centralizabar, .centralizacid, .centralizaest{
+                margin: 0;
+                position: absolute;
+                margin-right: -50%;
+                transform: translate(-50%, -50%);
+                width:200px;
+            }
+            .centralizacep{
+                width:80px;
+                top: 20%;
+                left: 50%;
+            }
+            .centralizabtn{
+                top: 30%;
+                left: 50%;
+            }
+            .centralizaend{
+                top: 55%;
+                left: 30%;
+            }
+            .centralizacomp{
+                top: 55%;
+                left: 50%;
+            }
+            .centralizabar{
+                top: 55%;
+                left: 70%;
+            }
+            .centralizacid{
+                top: 65%;
+                left: 40%;
+            }
+            .centralizaest{
+                top: 65%;
+                left: 60%;
+            }
+        </style>
     </head>
     <body>
-        <input type="text" name="cep" placeholder="CEP..." maxlength="8"></input>
+        
+        <input type="text" id="cep" name="cep" placeholder="CEP..." maxlength="8" class="centralizacep"></input>
+        <button id="btn" class="centralizabtn">Encontrar</button>
         <br><br>
-        <input readonly type="text" id="endereco" placeholder="Endereço..."></input>
-        <input readonly type="text" id="numero" placeholder="Número..."></input>
-        <input readonly type="text" id="bairro" placeholder="Bairro..."></input>
+        <input readonly type="text" id="endereco" placeholder="Endereço..." class="centralizaend" title="Endereço"></input>
+        <input readonly type="text" id="complemento" placeholder="Complemento..." class="centralizacomp" title="Complemento"></input>
+        <input readonly type="text" id="bairro" placeholder="Bairro..." class="centralizabar" title="Bairro"></input>
         <br><br>
-        <input readonly type="text" id="cidade" placeholder="Cidade..."></input>
-        <input readonly type="text" id="estado" placeholder="Estado..."></input>
+        <input readonly type="text" id="cidade" placeholder="Cidade..." class="centralizacid" title="Cidade"></input>
+        <input readonly type="text" id="estado" placeholder="Estado..." class="centralizaest" title="Estado"></input>
     </body>
 </html>
--- index.php
+++ index.php
@@ -9,5 +9,7 @@
 		<a href="20200819/Engenharia_Reversa/index.php">Exercicio Engenharia Reversa</a> <br>
 		<a href="20200825/CadastroAjax.php">Exercicio Frutas Ajax</a> <br>
 		<a href="20200902/index.php">Exercicio JSON</a> <br>
+		<br>
+		<a href="Trabalho_Bimestral/index.php">Trabalho Bimestral</a> <br>
 	</body>
 </html>
