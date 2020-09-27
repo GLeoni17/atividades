6cfed3df5954e24142553e118a6dea96872eff74
GLeoni17 <gabriel.lduarte@outlook.com>
2020-09-26 22:13:39 -0300

    Editado index.php para indicar as atividades

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 		<a href="20200819/Engenharia_Reversa/index.php">Exercicio Engenharia Reversa</a> <br>
 		<a href="20200825/CadastroAjax.php">Exercicio Frutas Ajax</a> <br>
 		<a href="20200902/index.php">Exercicio JSON</a> <br>
+		<a>Atividade MySQL com PHP - Taxonomia</a> <br>
+		<ul>
+			<li><a href="20200923/genero.php">Genero</a></li>
+			<li><a href="20200923/familia.php">Familia</a></li>
+			<li><a href="20200923/especie.php">Especie</a></li>
+		</ul>
 		<br>
 		<a href="Trabalho_Bimestral/index.php">Trabalho Bimestral</a> <br>
 	</body>
