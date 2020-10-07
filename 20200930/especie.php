40b12804d38d7d583326df382d5cd7ab135cc999
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-07 20:25:34 -0300

    Adicionado exericicio select 30 09 2020 e 07 10 2020

--- 20200930/especie.php
+++ 20200930/especie.php
@@ -12,7 +12,28 @@
 
     function table(){
         include "conexao.php";
-        $select="SELECT especie.nome as nome_especie, especie.nome_cientifico as nome_cientifico_especie, genero.nome_cientifico as genero_especie, familia.nome_cientifico as familia_especie FROM especie INNER JOIN genero ON especie.cod_genero=genero.id_genero INNER JOIN familia ON genero.cod_familia=familia.id_familia ORDER BY nome_especie, nome_cientifico_especie, genero_especie, familia_especie ";
+        $select="SELECT especie.nome AS nome_especie, especie.nome_cientifico AS nome_cientifico_especie, genero.nome_cientifico 
+        as genero_especie, familia.nome_cientifico as familia_especie
+        FROM especie INNER JOIN genero ON especie.cod_genero=genero.id_genero INNER JOIN familia ON genero.cod_familia=familia.id_familia";
+        if($_POST){
+            if($_POST["familia"]!= ""){
+                if($_POST["genero"] != ""){
+                    $select.= " and genero.id_genero = '".$_POST['genero']."'";
+                }else{
+                    $select.= " and familia.id_familia = '".$_POST['familia']."'";
+                }
+            }
+            $select.=" WHERE especie.nome LIKE '%".$_POST['nome']."%' OR especie.nome_cientifico LIKE '%".$_POST['nome']."%'";
+        }
+        $select.=" ORDER BY nome_especie, nome_cientifico_especie, genero_especie, familia_especie";
+        
+
+        /*as nome_familia, 
+            especie.nome_cientifico as nome_cientifico_especie, genero.nome_cientifico as genero_especie, 
+            familia.nome_cientifico as familia_especie FROM especie 
+            INNER JOIN genero ON especie.cod_genero=genero.id_genero 
+            INNER JOIN familia ON genero.cod_familia=familia.id_familia 
+            ORDER BY nome_especie, nome_cientifico_especie, genero_especie, familia_especie ";*/
         $res=mysqli_query($con, $select);
         while($linha=mysqli_fetch_assoc($res)){
             echo "<tr>";
@@ -28,7 +49,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <script src="jquery-3.5.1.min.js"></script>
+        <script src="../../../jquery-3.5.1.min.js"></script>
 
         <script>
             $(document).ready(function(){
@@ -36,7 +57,11 @@
                     //PHP pra buscar
                     var id = $("#familia").val();
                     $.post("seleciona_genero.php", {"id":id}, function(msg){
-
+                        option="<option label='SELECIONE O GENERO' />";
+                        $.each(msg, function(indice, valor){
+                            option+="<option value='"+valor.id_genero+"'> "+valor.nome_cientifico+" </option>";
+                        });
+                        $("#genero").html(option);
                     });
                 });
             });
@@ -44,17 +69,24 @@
 
     </head>
     <body>
-        <table border="1px">
+        
         <h3>Filtrar por:</h3>
-        <select id="familia">
-            <option value="">::SELECIONE UMA FAMÍLIA::</option>
-            <?php select(); ?>
-        </select>
-        <br><br>
-        <select id="genero">
-            <option>::SELECIONE UM GÊNERO::</option>
-        </select>
-
+        <div>   
+            <form action="especie.php" method="post">
+            <select id="familia" name="familia">
+                <option label="SELECIONE UMA FAMILIA" />
+                <?php select(); ?>
+            </select>
+            <br><br>
+            <select id="genero" name="genero">
+                <option label="SELECIONE UM GENERO" />
+            </select>
+        
+            <input type="text" name="nome" placeholder="Nome ou nome científico…."> 
+            <button>Pesquisar</button><br /> <hr />
+            </form>
+        </div>
+        <table border="1px">
             <tr >
                 <th colspan="4">Especie</th>
             </tr>
--- 20200930/seleciona_genero.php
+++ 20200930/seleciona_genero.php
@@ -1,16 +1,16 @@
 <?php 
+    header("Content-Type: application/json");
     include "conexao.php";
     // Select pra verificar nas familias
     // Precisamos do nome da familia do nome cientifico selecionado
 
-    $select="SELECT genero.nome_cientifico as nome_cientifico_genero
-
-    as nome_cientifico_familia FROM genero  
+    $select="SELECT nome_cientifico, id_genero FROM genero  
     
-    WHERE cod_familia = '".$_POST['id']."' ORDER BY nome_cientifico_genero";
+    WHERE cod_familia = '".$_POST['id']."' ORDER BY nome_cientifico";
 
     $res = mysqli_query($con, $select);
     while($linha=mysqli_fetch_assoc($res)){
-        echo $linha["nome_cientifico"];
+        $resultado[]= $linha;
     }
+    echo json_encode($resultado);
 ?>
